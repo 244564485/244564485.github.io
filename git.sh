7b9c5ec7e0f53e5b0eee09e77fea4bca4c9ed8d6
244564485@qq.com <244564485@qq.com>
2023-05-05 14:40:42 +0800

    20230505 14:40:42

--- git.sh
+++ git.sh
@@ -1,5 +1,8 @@
+
 git add .
 git commit -m "`date -d "+0 hour" '+%Y%m%d %H:%M:%S'`"
 git pull --rebase origin main 
 git push -u origin main
+
+cp -rf hw/homework.md //wsl.localhost/Ubuntu-20.04/home/at/test/jupyter/settings/browser/www/  
 
